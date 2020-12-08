021f3438d5246e82958d6b8cc4ff9489aa4cec8a
gx1989mao <gx1989mao@gmail.com>
2020-12-08 13:10:01 +0800

    update

---
+++ source/__pycache__/discode.py
@@ -0,0 +1,11 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 3.8 (3413)
+# Decompiled from: Python 3.8.3 (default, Jul  2 2020, 17:30:36) [MSC v.1916 64 bit (AMD64)]
+# Embedded file name: ./source\bytecodeshow.py
+# Compiled at: 2020-12-07 20:56:43
+# Size of source mod 2**32: 22 bytes
+
+
+def hello():
+    pass
+# okay decompiling ./source/__pycache__\bytecodeshow.cpython-38.pyc
